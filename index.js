71e878141580c31cdebba67411d2382a0788ca9a
Jairo Vera chaly <jairoverachaly@gmail.com>
2020-08-27 20:59:35 -0400

    correccion de PORT

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes/index')
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const path = require('path');
 const bodyParser = require('body-parser');
 //const expressValidator = require('express-validator');
